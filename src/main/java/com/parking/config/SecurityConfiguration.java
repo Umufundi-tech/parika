20718ca33cf3c568bd4ddb1ba3360b25e55b47df
IgirubElam <igirubuntuelam@gmail.com>
2024-10-23 12:36:14 +0200

    other change in securityconfiguration

--- src/main/java/com/parking/config/SecurityConfiguration.java
+++ src/main/java/com/parking/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 	    return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+	    		.cors(Customizer.withDefaults()) // Enable CORS
 	            .authorizeHttpRequests(auth -> auth
 	                    .requestMatchers(
 	                            new AntPathRequestMatcher("/auth/login"),
